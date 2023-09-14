fab627a955429ec6aa39d2cdf2a22ff7214e18e7
Nick Koerber <thisisbilly1@gmail.com>
2023-09-13 20:44:06 -0500

    post auth

--- src/helpers/cameraService.js
+++ src/helpers/cameraService.js
@@ -19,6 +19,7 @@ export async function post(path, body) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: authService.accessToken,
     },
     body: JSON.stringify(body),
   })
